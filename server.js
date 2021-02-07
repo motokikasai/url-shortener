65cd983b05677e7dd1ea8389a69995e7acad84a2
motokikasai <kmomoky@hotmail.com>
2021-02-07 23:16:17 +0100

    Create model and add logic & redirect

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrls');
 const app = express();
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // set the view engine with ejs
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render('index', { shortUrls: shortUrls });
+});
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+
+  res.redirect('/');
+});
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
 });
 
 const port = process.env.PORT || 5000;
